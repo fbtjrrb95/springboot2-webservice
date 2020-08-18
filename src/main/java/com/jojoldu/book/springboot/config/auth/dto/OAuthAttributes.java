60b37f562847955eaf8a2b192ac79d1f39d21623
seokkyu_ryu <tjrrb95@gmail.com>
2020-08-18 19:57:37 +0900

    2020-08-18 social Login#1 (google)

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,47 @@
+package com.jojoldu.book.springboot.config.auth.dto;
+
+import com.jojoldu.book.springboot.domain.user.Role;
+import com.jojoldu.book.springboot.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributesKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributesKey, String name, String email, String picture){
+        this.attributes = attributes;
+        this.name = name;
+        this.nameAttributesKey = nameAttributesKey;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public static OAuthAttributes of(String registrationId, String userNmaeAttributeName, Map<String, Object> attributes){
+        return OAuthAttributes.builder()
+                .name((String)attributes.get("name"))
+                .email((String)attributes.get("email"))
+                .picture((String)attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributesKey(userNmaeAttributeName)
+                .build();
+    }
+
+
+    public User toEntity(){
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+    }
+
+}
